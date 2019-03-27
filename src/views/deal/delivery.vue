64b9526cdd5fff93e39c63af9c62cea24f5d5249
jiangdaye <692466154@qq.com>
2019-03-27 23:45:59 +0800

    交易提货页面接口联调

--- src/views/deal/delivery.vue
+++ src/views/deal/delivery.vue
@@ -13,49 +13,66 @@
     <div class="delivery-middle">
       <div>本次提货数量</div>
       <van-field
+        type="number"
         placeholder="您本次提货数量"
         clearable
-        v-model="formData.deliveryNum"
+        v-model="formData.num"
       >
       </van-field>
       <div>您的钱包地址</div>
       <van-field
         placeholder="您的钱包地址"
         clearable
-        v-model="formData.deliveryNum"
+        v-model="formData.address"
       >
       </van-field>
       <div>您的帐户姓名</div>
       <van-field
         placeholder="您的帐户姓名"
         clearable
-        v-model="formData.deliveryNum"
+        v-model="formData.accountName"
       >
       </van-field>
     </div>
     <div class="delivery-bottom">
-      <input type="checkbox" name="xieyi" value="">
+      <input type="checkbox" accountName="xieyi" value="" v-model="isCheck">
       我同意提货协议
     </div>
     <div class="submit_buttons">
-      <van-button type="primary" block @click="submit">提交审核</van-button>
+      <van-button type="primary" block @click="submit" :disabled="!isCheck">提交审核</van-button>
     </div>
   </com-page>
 </template>
 
 <script>
+  import { Toast } from "vant"
+  import { pickGoods } from "@/api/index"
+  import { paramConvert } from "@/utils/stringUtil.js"
   export default {
     data () {
       return {
         formData: {
-          deliveryNum: '',  //提货数量
-          walletAddress: '',  //钱包地址
-          name: ''  //帐户姓名
-        }
+          id: '',  //用户id
+          num: '',  //提货数量
+          address: '',  //钱包地址
+          accountName: '',  //帐户姓名
+          shopId: ''  //钱包地址
+        },
+        isCheck: false
       }
     },
 
+    created () {
+      this.formData.id = this.userId || ''
+      this.formData.shopId = this.deliveryList.id || ''
+    },
+
     computed: {
+      userId: {
+        get: function () {
+          return this.$store.getters.getUserId
+        }
+      },
       deliveryList: {
         get: function () {
           return this.$store.getters.getDeliveryList
@@ -65,7 +82,30 @@
 
     methods: {
       submit () {
-        
+        if (!this.isCheck) { return } 
+        if (this.formData.num === '') {
+          Toast('提货数量不能为空')
+          return
+        }
+        if (this.formData.address === '') {
+          Toast('钱包地址不能为空')
+          return
+        }
+        if (this.formData.accountName === '') {
+          Toast('帐户姓名不能为空')
+          return
+        }
+        this.pickGoods()
+      },
+      async pickGoods () {
+        let queryParams = paramConvert(this.formData)
+        let resData = await pickGoods(queryParams, this.formData)
+        if (resData.status === 200 && resData.data.Success) {
+          Toast("提交成功")
+          this.$router.push("/deal")
+        } else {
+          Toast(resData.data.Msg)
+        }
       }
     }
   }
@@ -86,6 +126,14 @@
   }
   .delivery-bottom {
     padding: 30px 15px 0 15px;
+  }
+  .delivery-btn-checked {
+    background-color: #ec9300;
+    border: 0.02rem solid #ec9300
+  }
+  .delivery-not-checked {
+    background-color: #ccc;
+    border: 0.02rem solid #ccc;
   }
 </style>
   
