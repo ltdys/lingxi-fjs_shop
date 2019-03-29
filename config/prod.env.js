f5a562434eb514238c2527bbb00ad7792aacf4c9
ltdys <692466154@qq.com>
2019-03-29 17:42:48 +0800

    修改打包路径

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,7 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   //api 跟路径
-  BASE_API: '"http://sfd.witrh.com"',
+  BASE_API: '"http://zzlh.api.witrh.com"',
   //webscoket url
   WS_URL: '"ws://sfd.witrh.com:10"',
   //用户端下载URL
